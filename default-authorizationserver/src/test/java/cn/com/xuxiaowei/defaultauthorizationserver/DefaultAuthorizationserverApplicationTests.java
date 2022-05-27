35c485bfce983451878933a5bd4bc7585262e120
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-05-27 12:46:37 +0800

    add default-authorizationserver

---
+++ default-authorizationserver/src/test/java/cn/com/xuxiaowei/defaultauthorizationserver/DefaultAuthorizationserverApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei.defaultauthorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefaultAuthorizationserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
