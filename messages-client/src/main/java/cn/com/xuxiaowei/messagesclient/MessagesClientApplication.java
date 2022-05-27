d1d2f15fa224f20fe5151bcb846cf3aa86b08bfb
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-05-27 12:59:22 +0800

    add messages-client

---
+++ messages-client/src/main/java/cn/com/xuxiaowei/messagesclient/MessagesClientApplication.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei.messagesclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagesClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagesClientApplication.class, args);
+	}
+
+}
