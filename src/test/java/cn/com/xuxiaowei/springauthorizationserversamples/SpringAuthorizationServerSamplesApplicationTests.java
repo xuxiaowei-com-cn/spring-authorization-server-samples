887d1486418ad6e3e5c17a7bd5b4e3fd2bf36e13
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-05-27 12:34:55 +0800

    init

---
+++ src/test/java/cn/com/xuxiaowei/springauthorizationserversamples/SpringAuthorizationServerSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei.springauthorizationserversamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthorizationServerSamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
